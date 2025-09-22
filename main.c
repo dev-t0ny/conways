b584b17035c0371057daa3f9b6a7a5a10271f1a1
dev-t0ny <antoineboudreau@Outlook.com>
2025-09-22 11:45:25 -0400

    basic game loop

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #define HEIGHT 20
 #define WIDTH 50
@@ -11,11 +12,23 @@ int main()
 {
     int generation = 0;
     bool grid[HEIGHT][WIDTH];
+    
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for(int j = 0; j < WIDTH; j++)
+        {
+            grid[i][j] = false;
+        }
+    }
 
     while (1)
     {
         generation++;
-        update(generation);
+
+        update(generation, grid);
+        render(generation, grid);
+        usleep(100000); 
+
     }
     
     return 0;
